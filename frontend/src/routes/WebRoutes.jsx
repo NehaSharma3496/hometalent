cef1e3d57018c76cbec346d39f04eaabc0b97014
NehaSharma3496 <sharmaneha3496@gmail.com>
2025-07-01 19:04:48 +0530

    Remove unnecessary blank line in WebRoutes component for cleaner code

--- frontend/src/routes/WebRoutes.jsx
+++ frontend/src/routes/WebRoutes.jsx
@@ -47,7 +47,6 @@ const WebRoutes = (
     <Route path="/about" element={<About />} />
     <Route path="/contact" element={<Contact />} />
     <Route path="/gallery" element={<Gallery />} />
- 
     <Route path='/blog' element={<Blog/>}/>
     <Route path="/registration" element={<Registration />} />
     <Route path="/faq" element={<Faq/>}/>
