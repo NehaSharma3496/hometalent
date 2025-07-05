ec55116554d0bb226e33ea93bb169fa83845896e
NehaSharma3496 <sharmaneha3496@gmail.com>
2025-07-05 16:39:22 +0530

    feat: implement vendor registration form with validation and API integration; add category, state, and city fetching

---
+++ frontend/src/Services/vendor/Vendor.js
@@ -0,0 +1,104 @@
+import axios from "axios";
+
+import * as Config from "../../Utils/config";
+
+// export async function LoginApi(data) {
+//   try {
+//     const response = await axios.post(`${Config.base_url}user/login`, data);
+
+//     return response.data;
+//   } catch (error) {
+//     console.log("Error fetching login:", error.message || error);
+//     throw error;
+//   }
+// }
+
+// export async function UserLoginApi(data) {
+//   try {
+//     const response = await axios.post(
+//       `${Config.base_url}api/client/login`,
+//       data
+//     );
+
+//     return response.data;
+//   } catch (error) {
+//     console.log("Error fetching login:", error.message || error);
+//     throw error;
+//   }
+// }
+
+// export async function UserSignupApi(data) {
+//   try {
+//     const response = await axios.post(`${Config.base_url}api/client/add`, data);
+
+//     return response?.data;
+//   } catch (error) {
+//     // console.log("Error fetching login:", error.response.data.message || error);
+//     throw error;
+//   }
+// }
+
+
+// export async function UserOtpSubmit(data) {
+//   try {
+//     const response = await axios.post(`${Config.base_url}api/client/otp_submit`, data);
+
+//     return response.data;
+//   } catch (error) {
+//     console.log("Error fetching login:", error.message || error);
+//     throw error;
+//   }
+// }
+
+
+export async function VendorRegister(data) {
+  try {
+    const response = await axios.post(`${Config.base_url}addUser`, data);
+    return response;
+  } catch (error) {
+    throw error?.response?.data || error;
+  }
+}
+
+
+export async function GetCategories(token) {
+    try {
+        const res = await axios.get(`${Config.base_url}vendor/categories`, {
+            headers: {
+                'Authorization': `${token}`
+            },
+        });
+
+        return res?.data;
+    } catch (err) {
+        return err;
+    }
+}
+
+export async function GetStates(token) {
+    try {
+        const res = await axios.get(`${Config.base_url}vendor/states`, {
+            headers: {
+                'Authorization': `${token}`
+            },
+        });
+
+        return res?.data;
+    } catch (err) {
+        return err;
+    }
+}
+
+export async function GetCities(token, stateId) {
+  try {
+    const res = await axios.get(`${Config.base_url}vendor/cities?state_id=${stateId}`, {
+      headers: {
+        'Authorization': `${token}`
+      },
+    });
+
+    return res?.data;
+  } catch (err) {
+    return err;
+  }
+}
