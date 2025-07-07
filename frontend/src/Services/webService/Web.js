a1cf9d9f67f24f853b59b65e872deb27eff77220
jatshakti744 <er.shaktijat@gmail.com>
2025-07-06 23:18:16 -0700

    done

--- frontend/src/Services/webService/Web.js
+++ frontend/src/Services/webService/Web.js
@@ -1,2 +1,15 @@
 import axios from "axios";
-import * as Config from "../../Utils/"
+import * as Config from "../../Utils/config";
+
+export async function getcategory(token) {
+    try {
+        const res = await axios.get(`${Config.base_url}vendor/categories`, {
+            headers: {
+                'Authorization': `${token}`
+            },
+        });
+        return res?.data;
+    } catch (err) {
+        return err;
+    }
+}
