8241ce1cdfffff8e449a66895e91b1ea078e9ed9
NehaSharma3496 <sharmaneha3496@gmail.com>
2025-07-11 19:18:12 +0530

    done

--- frontend/src/Utils/config.js
+++ frontend/src/Utils/config.js
@@ -3,8 +3,11 @@ export const Doamin_url = `${window.location.origin}/`;
 
 
 
+// export const base_url = `http://localhost:9999/`;
 export const base_url = `http://localhost:9999/`;
-export const soket_url = `http://localhost:9999/`;
+// export const soket_url = `http://localhost:9999/`;
+
+
 
 
 
