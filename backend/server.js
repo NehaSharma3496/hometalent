54a0421287284dcdcffa20373ecf60fe24ca7169
jatshakti744 <er.shaktijat@gmail.com>
2025-07-14 12:46:52 +0530

    done

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const app = express();
 // };
 
 const corsOptions = {
-  origin: ["http://localhost:3002", "*"],
+  origin: ["http://localhost:3000", "*"],
   credentials: true,
   methods: ["GET", "POST"],
   allowedHeaders: ["Content-Type", "Authorization", "x-access-token"],
