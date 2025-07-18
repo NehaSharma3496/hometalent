53b992dcf76e0ed5d2adce560e1578dd228206c3
jatshakti744 <er.shaktijat@gmail.com>
2025-07-18 11:51:09 +0530

    done

--- frontend/src/routes/VendorRoutes.jsx
+++ frontend/src/routes/VendorRoutes.jsx
@@ -11,7 +11,7 @@ import ViewGallery from "../pages/vendor/gallery/ViewGallery";
 import UploadGallery from "../pages/vendor/gallery/UploadGallery";
 
 import Allvendors from "../pages/admin/vendor/Allvendors";
-
+import MyProfile from "../components/admincomponents/MyProfile";
 
 // for vendor routing
 
@@ -22,8 +22,9 @@ const AdminRoutes = (
     <Route path="users/adduser" element={<Adduser />} />
     <Route path="users/blocked" element={<BlockedUser />} />
     <Route path="users/feedback" element={<FeedbackandReview />} />
-    <Route path="gallery" element={<ViewGallery/>}/>
-    <Route path="gallery/upload" element={<UploadGallery/>}/>
+    <Route path="gallery" element={<ViewGallery />} />
+    <Route path="gallery/upload" element={<UploadGallery />} />
+    <Route path="myprofile" element={<MyProfile />} />
     {/* <Route path="clients" element={<Clients />} /> */}
   </Route>
 );
