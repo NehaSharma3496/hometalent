4d34f55d7de9d4909ed5fa147ba241b878afbc84
jatshakti744 <er.shaktijat@gmail.com>
2025-07-22 14:00:05 +0530

    done

--- frontend/src/Utils/config.js
+++ frontend/src/Utils/config.js
@@ -1,18 +1,8 @@
 export const Doamin_url = `${window.location.origin}/`;
 
-
-
-
 // export const base_url = `http://localhost:9999/`;
-export const base_url = `http://localhost:8888/`;
+export const base_url = `http://217.145.69.32:8888`;
 // export const soket_url = `http://localhost:9999/`;
-
-
-
-
-
-
-
 
 // export const soket_url = `${window.location.origin}:1001/`;
 // export const base_url = `${window.location.origin}/backend/`
