807c1c81b299a22abc86950c6712f9af90b8abec
Nilesh-123-n <nnagar1314@gmail.com>
2025-08-05 12:51:43 +0530

    done

--- backend/app/routes/authRoutes.js
+++ backend/app/routes/authRoutes.js
@@ -10,6 +10,7 @@ router.post('/addUser', uploadMedia, authController.createUser);
 router.post('/addUser', uploadMedia, authController.createUser);
 router.post('/forgotPassword', authController.forgotPassword);
 router.post('/resetPassword', authController.resetPassword);
+router.post('/change_password', authController.reset_password);
 
 
 
