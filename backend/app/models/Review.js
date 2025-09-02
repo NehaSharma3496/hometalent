2decf2cd00be2f57e6debbc2620e7c48d7ee6b7b
Nilesh-123-n <nnagar1314@gmail.com>
2025-09-02 18:17:13 +0530

    done

--- backend/app/models/Review.js
+++ backend/app/models/Review.js
@@ -9,6 +9,12 @@ const sequelize = require('../config/db.config');
       type: DataTypes.TEXT,
       allowNull: false,
     },
+
+    rating: {
+        type: DataTypes.INTEGER,
+        allowNull: true
+    },
+
     status: {
         type: DataTypes.INTEGER,
         allowNull: false,
