d46cc50f25e658f866a3950045f11965269266b1
anuj-8109 <anujpaterya29@gmaail.com>
2025-09-17 12:20:29 +0530

    Done

--- frontend/src/components/admincomponents/AdminHeader.jsx
+++ frontend/src/components/admincomponents/AdminHeader.jsx
@@ -137,46 +137,48 @@ export default function AdminHeader() {
     notifications.length > 0 ? notifications : storedNotifications;
 
 
-  // useEffect(() => {
-  //   if (window.innerWidth > 576) return;
-  //   const links = document.querySelectorAll(".sidebar-link");
-
-  //   const handleClick = () => {
-  //     document.body.classList.add("sidebar-toggle");
-  //   };
-
-  //   links.forEach(link => {
-  //     link.addEventListener("click", handleClick);
-  //   });
-
-  //   return () => {
-  //     links.forEach(link => {
-  //       link.removeEventListener("click", handleClick);
-  //     });
-  //   };
-  // }, []);
-
-  const sidebarRef = useRef(null);
-
   useEffect(() => {
+    if (window.innerWidth > 576) return;
+    const links = document.querySelectorAll(".sidebar-link");
 
-    const handleClickOutside = (e) => {
-
-      if (window.innerWidth > 576) return;
-      const sidebar = document.getElementById("sidebar");
-      const clickedInsideSidebar = sidebar?.contains(e.target);
-      const clickedToggleBtn = document
-        .querySelector(".toggle-sidebar-btn")
-        ?.contains(e.target);
-      if (!clickedInsideSidebar && !clickedToggleBtn) {
-        document.body.classList.toggle("sidebar-toggle");
-      }
-
+    const handleClick = () => {
+      document.body.classList.add("sidebar-toggle");
     };
 
-    document.addEventListener("mousedown", handleClickOutside);
-    return () => document.removeEventListener("mousedown", handleClickOutside);
+    links.forEach(link => {
+      link.addEventListener("click", handleClick);
+    });
+
+    return () => {
+      links.forEach(link => {
+        link.removeEventListener("click", handleClick);
+      });
+    };
   }, []);
+
+const sidebarRef = useRef(null);
+
+useEffect(() => {
+  const handleClickOutside = (e) => {
+    if (window.innerWidth > 576) return;
+
+    const sidebar = document.getElementById("sidebar");
+    const clickedInsideSidebar = sidebar?.contains(e.target);
+    const clickedToggleBtn = document
+      .querySelector(".toggle-sidebar-btn")
+      ?.contains(e.target);
+
+    if (!clickedInsideSidebar && !clickedToggleBtn) {
+      
+      document.body.classList.add("sidebar-toggle");
+    }
+  };
+
+  document.addEventListener("mousedown", handleClickOutside);
+  return () => document.removeEventListener("mousedown", handleClickOutside);
+}, []);
+
+
 
 
 
