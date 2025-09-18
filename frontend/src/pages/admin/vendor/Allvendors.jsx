d756b3e2fe3df78041d09b10b364da4c07191959
jatshakti744 <er.shaktijat@gmail.com>
2025-09-18 14:44:08 +0530

    done

--- frontend/src/pages/admin/vendor/Allvendors.jsx
+++ frontend/src/pages/admin/vendor/Allvendors.jsx
@@ -665,9 +665,12 @@ if (packageFilter) {
       <div className="row align-items-center mb-3">
         <div className="col-md-6">
           <div className="add-page-heading-div">
-            <Link to="/admin/dashboard">
+             <button
+              className="btn btn-link p-0"
+              onClick={() => navigate(-1)}  // 🔹 पिछली history में वापस जाएगा
+            >
               <i className="fa-sharp fa-regular fa-arrow-left"></i>
-            </Link>
+            </button>
             <h2 className="add-page-heading">All Vendor</h2>
           </div>
         </div>
