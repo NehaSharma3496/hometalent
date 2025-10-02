6407713785a3a45819abb9b805782d62e78108ea
jatshakti744 <er.shaktijat@gmail.com>
2025-10-02 16:22:34 +0530

    done

--- frontend/src/Utils/config.js
+++ frontend/src/Utils/config.js
@@ -1,6 +1,6 @@
 export const Doamin_url = `${window.location.origin}/`;
 
-export const base_url = `http://localhost:8888/`;
+export const base_url = `http://localhost:8888/`;s
 export const image_baseurl = `http://localhost:8888/`;
 export const socket_url = `http://localhost:8888/`;
 // export const base_url = `http://217.145.69.32:8888/`;
